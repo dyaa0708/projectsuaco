450bd4307673ea1ad46d6f96a6446bed0f81771d
dyaa0708 <lidyamunthe07@gmail.com>
2024-08-12 09:26:52 +0700

    first commit

---
+++ cafe suaco fix/gudang.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Des 2023 pada 09.09
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id_barang` varchar(5) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis` varchar(255) NOT NULL,
+  `stok_minimum` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `satuan_barang` varchar(255) NOT NULL,
+  `pemasok` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `jenis`, `stok_minimum`, `stok`, `satuan_barang`, `pemasok`) VALUES
+('B0001', 'kopiii', 'minuman', 2, 1, 'packs', 'toko mulya (082386333435)'),
+('B0002', 'teh', 'minuman', 2, 0, 'packs', 'toko bismillah');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang_keluar`
+--
+
+CREATE TABLE `tbl_barang_keluar` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `barang` varchar(5) NOT NULL,
+  `jumlah` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang_keluar`
+--
+
+INSERT INTO `tbl_barang_keluar` (`id_transaksi`, `tanggal`, `barang`, `jumlah`) VALUES
+('LR-11111', '2023-12-04', 'B0001', 3),
+('LR-11112', '2023-12-05', 'B0001', 5);
+
+--
+-- Trigger `tbl_barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus_stok_keluar` BEFORE DELETE ON `tbl_barang_keluar` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok+OLD.jumlah
+WHERE id_barang=OLD.barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_keluar` AFTER INSERT ON `tbl_barang_keluar` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok-NEW.jumlah
+WHERE id_barang=NEW.barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang_masuk`
+--
+
+CREATE TABLE `tbl_barang_masuk` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `barang` varchar(5) NOT NULL,
+  `jumlah` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang_masuk`
+--
+
+INSERT INTO `tbl_barang_masuk` (`id_transaksi`, `tanggal`, `barang`, `jumlah`) VALUES
+('LS-11111', '2023-12-04', 'B0001', 5),
+('LS-11112', '2023-12-05', 'B0001', 4);
+
+--
+-- Trigger `tbl_barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus_stok_masuk` BEFORE DELETE ON `tbl_barang_masuk` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok-OLD.jumlah
+WHERE id_barang=OLD.barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_masuk` AFTER INSERT ON `tbl_barang_masuk` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok+NEW.jumlah
+WHERE id_barang=NEW.barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jenis`
+--
+
+CREATE TABLE `tbl_jenis` (
+  `id_jenis` int(11) NOT NULL,
+  `nama_jenis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_jenis`
+--
+
+INSERT INTO `tbl_jenis` (`id_jenis`, `nama_jenis`) VALUES
+(2, 'bubuk');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `nama_satuan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hak_akses` enum('Administrator','Admin Gudang','Kepala Gudang') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `hak_akses`) VALUES
+(1, 'Admin', 'administrator', '$2y$12$Yi/I5f1jPoQNQnh6lWoVfuz.RtZ3OHcKN6PU.I62P0fYK1tJ7xMRi', 'Administrator'),
+(2, 'Admin Gudang', 'admin gudang', '$2y$12$BeRYh13zfPXej97VgcfeNucYJGTElha5sRyIUQm1278D2u2Aqf6DS', 'Admin Gudang'),
+(3, 'Karyawan Nila', 'nila', '$2y$12$DlTZWqEuXiNxy2zeIzSNb.jjvsIT6rPNzg.9WUJ/CAwNTMgdH8Dqm', 'Admin Gudang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `tbl_barang_keluar`
+--
+ALTER TABLE `tbl_barang_keluar`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `barang` (`barang`);
+
+--
+-- Indeks untuk tabel `tbl_barang_masuk`
+--
+ALTER TABLE `tbl_barang_masuk`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `barang` (`barang`);
+
+--
+-- Indeks untuk tabel `tbl_jenis`
+--
+ALTER TABLE `tbl_jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indeks untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_jenis`
+--
+ALTER TABLE `tbl_jenis`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_barang_keluar`
+--
+ALTER TABLE `tbl_barang_keluar`
+  ADD CONSTRAINT `tbl_barang_keluar_ibfk_1` FOREIGN KEY (`barang`) REFERENCES `tbl_barang` (`id_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_barang_masuk`
+--
+ALTER TABLE `tbl_barang_masuk`
+  ADD CONSTRAINT `tbl_barang_masuk_ibfk_1` FOREIGN KEY (`barang`) REFERENCES `tbl_barang` (`id_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
